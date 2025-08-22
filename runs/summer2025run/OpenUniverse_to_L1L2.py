c9ddc6c0ce6879fd452f814507a6fe2b09d96480
hirata10 <hirata.10@osu.edu>
2025-08-21 21:19:55 -0400

    update summer2025run scripts to LINCC standards

--- runs/summer2025run/OpenUniverse_to_L1L2.py
+++ runs/summer2025run/OpenUniverse_to_L1L2.py
@@ -1,153 +1,157 @@
-"""Driver to process lots of sims.
-"""
+"""Driver to process lots of sims."""
 
-import sys
 import os
-import io
-from contextlib import redirect_stdout
-from multiprocessing import Pool
-import datetime
+import sys
 
 from ...from_sim import sim_to_isim
-from ...L1_to_L2 import gen_cal_image, gen_noise_image
+from ...L1_to_L2 import gen_noise_image
 from ...utils import maskhandling
 
 ### --- Setup information ---
+
 
 # helper function to get commands of the form --keychar=value
 def _getval(keychar, default=None):
     n = len(keychar)
     for a in sys.argv[1:]:
-        if a[:n+3] == '--'+keychar+'=':
-           return a[n+3:]
+        if a[: n + 3] == "--" + keychar + "=":
+            return a[n + 3 :]
     return default
 
-input_dir = _getval('in')
-output_dir = _getval('out', default='.')
-cal_dir = _getval('cal')
-tag = _getval('tag')
-seed = int(_getval('seed', default='500'))
-dseed = int(_getval('dseed', default='10'))
-temp_dir = os.getenv('TMPDIR', default=output_dir+'/L2')
-use_sca = int(_getval('sca', default='1'))
-Nmax = int(_getval('nmax', default='999')) # maximum number of chips to build
 
-print('arguments:')
-print('  input_dir =', input_dir)
-print('  output_dir =', output_dir)
-print('  cal_dir =', cal_dir)
-print('  tag =', tag)
-print('  seed =', seed)
-print('  dseed =', dseed)
-print('  temp_dir =', temp_dir)
-print('  use_sca =', use_sca)
-print('  Nmax =', Nmax)
+input_dir = _getval("in")
+output_dir = _getval("out", default=".")
+cal_dir = _getval("cal")
+tag = _getval("tag")
+seed = int(_getval("seed", default="500"))
+dseed = int(_getval("dseed", default="10"))
+temp_dir = os.getenv("TMPDIR", default=output_dir + "/L2")
+use_sca = int(_getval("sca", default="1"))
+Nmax = int(_getval("nmax", default="999"))  # maximum number of chips to build
+
+print("arguments:")
+print("  input_dir =", input_dir)
+print("  output_dir =", output_dir)
+print("  cal_dir =", cal_dir)
+print("  tag =", tag)
+print("  seed =", seed)
+print("  dseed =", dseed)
+print("  temp_dir =", temp_dir)
+print("  use_sca =", use_sca)
+print("  Nmax =", Nmax)
 sys.stdout.flush()
 
 ### --- now we have the information for this run ---
 
 # space seeds for SCAs
 nsca = 18
-seed += dseed*use_sca
+seed += dseed * use_sca
 
 # make directories if SCA 1
-if use_sca==1:
+if use_sca == 1:
     try:
-        os.mkdir(output_dir + '/L1')
-    except:
-        print('L1 directory already exists ...')
+        os.mkdir(output_dir + "/L1")
+    except FileExistsError:
+        print("L1 directory already exists ...")
     try:
-        os.mkdir(output_dir + '/L2')
-    except:
-        print('L2 directory already exists ...')
+        os.mkdir(output_dir + "/L2")
+    except FileExistsError:
+        print("L2 directory already exists ...")
 
-# useful function for finding calibration files
-def findcal(ctype,sca):
+
+def findcal(ctype, sca):
+    """Helper function for finding calibration files."""
+
     ctype_ = ctype
-    if ctype=='flat': ctype_='pflat' # right now we don't have the L-flats
-    return cal_dir + '/roman_wfi_' + ctype_ + '_' + tag + '_SCA{:02d}.asdf'.format(sca)
+    if ctype == "flat":
+        ctype_ = "pflat"  # right now we don't have the L-flats
+    return cal_dir + "/roman_wfi_" + ctype_ + "_" + tag + f"_SCA{sca:02d}.asdf"
+
 
 # figure out list of input files
 runlist = []
 outputs = []
-j=0
+j = 0
 for infile in os.listdir(input_dir):
-    if infile[-5:].lower()!='.fits':
+    if infile[-5:].lower() != ".fits":
         continue
 
     # get (obsid,sca) from file name
-    arr = infile.split('_')
+    arr = infile.split("_")
     band = arr[-3]
     obsid = int(arr[-2])
     sca = int(arr[-1][:-5])
 
-    if sca!=use_sca: continue
+    if sca != use_sca:
+        continue
 
     # now we need to process this file
-    print('\nProcessing: '+infile+'  obs={:d},sca={:d}'.format(obsid,sca))
+    print("\nProcessing: " + infile + f"  obs={obsid:d},sca={sca:d}")
 
     # Level 1 config
     caldir = {}
-    ctypes = ['linearitylegendre', 'gain', 'dark', 'read', 'ipc4d', 'flat', 'biascorr']
+    ctypes = ["linearitylegendre", "gain", "dark", "read", "ipc4d", "flat", "biascorr"]
     for ctype in ctypes:
-        caldir[ctype] = findcal(ctype,sca)
+        caldir[ctype] = findcal(ctype, sca)
     cfgs1 = {
-        'IN': input_dir + '/' + infile,
-        'OUT': output_dir + '/L1/sim_L1_{:s}_{:d}_{:d}.asdf'.format(band,obsid,sca),
-        'READS': [0, 1, 1, 2, 2, 4, 4, 10, 10, 26, 26, 32, 32, 34, 34, 35],
-        'FITSOUT': False,
-        'CALDIR': caldir,
-        'CNORM': 1.0,
-        'SEED': seed
+        "IN": input_dir + "/" + infile,
+        "OUT": output_dir + f"/L1/sim_L1_{band:s}_{obsid:d}_{sca:d}.asdf",
+        "READS": [0, 1, 1, 2, 2, 4, 4, 10, 10, 26, 26, 32, 32, 34, 34, 35],
+        "FITSOUT": False,
+        "CALDIR": caldir,
+        "CNORM": 1.0,
+        "SEED": seed,
     }
-    seed += dseed*nsca
+    seed += dseed * nsca
 
     # Level 2 config
     caldir = {}
-    ctypes = ['saturation', 'linearitylegendre', 'gain', 'dark', 'read', 'ipc4d', 'flat', 'biascorr', 'mask']
+    ctypes = ["saturation", "linearitylegendre", "gain", "dark", "read", "ipc4d", "flat", "biascorr", "mask"]
     for ctype in ctypes:
-        caldir[ctype] = findcal(ctype,sca)
+        caldir[ctype] = findcal(ctype, sca)
     cfgs2 = {
-        'IN': output_dir + '/L1/sim_L1_{:s}_{:d}_{:d}.asdf'.format(band,obsid,sca),
-        'OUT': output_dir + '/L2/sim_L2_{:s}_{:d}_{:d}.asdf'.format(band,obsid,sca),
-        'FITSWCS': output_dir + '/L1/sim_L1_{:s}_{:d}_{:d}_asdf_wcshead.txt'.format(band,obsid,sca),
-        'CALDIR': caldir,
-        'RAMP_OPT_PARS': {'slope': 0.4, 'gain': 1.8, 'sigma_read': 7.},
-        'JUMP_DETECT_PARS': {'SthreshA': 5.5, 'SthreshB': 4.5, 'IthreshA': 0.6, 'IthreshB': 600.},
-        'SKYORDER': 2,
-        'FITSOUT': False,
-        'NOISE': {
-            'LAYER': ['Rz4S2C1', 'Rz4S2C2'],
-            'TEMP': temp_dir + '/temp_{:s}_{:d}_{:d}.asdf'.format(band,obsid,sca),
-            'SEED': seed,
-            'OUT': output_dir + '/L2/sim_L2_{:s}_{:d}_{:d}_noise.asdf'.format(band,obsid,sca)
-        }
+        "IN": output_dir + f"/L1/sim_L1_{band:s}_{obsid:d}_{sca:d}.asdf",
+        "OUT": output_dir + f"/L2/sim_L2_{band:s}_{obsid:d}_{sca:d}.asdf",
+        "FITSWCS": output_dir + f"/L1/sim_L1_{band:s}_{obsid:d}_{sca:d}_asdf_wcshead.txt",
+        "CALDIR": caldir,
+        "RAMP_OPT_PARS": {"slope": 0.4, "gain": 1.8, "sigma_read": 7.0},
+        "JUMP_DETECT_PARS": {"SthreshA": 5.5, "SthreshB": 4.5, "IthreshA": 0.6, "IthreshB": 600.0},
+        "SKYORDER": 2,
+        "FITSOUT": False,
+        "NOISE": {
+            "LAYER": ["Rz4S2C1", "Rz4S2C2"],
+            "TEMP": temp_dir + f"/temp_{band:s}_{obsid:d}_{sca:d}.asdf",
+            "SEED": seed,
+            "OUT": output_dir + f"/L2/sim_L2_{band:s}_{obsid:d}_{sca:d}_noise.asdf",
+        },
     }
-    runlist.append((j,cfgs1,cfgs2))
+    runlist.append((j, cfgs1, cfgs2))
 
-    seed += dseed*nsca
-    j+=1
+    seed += dseed * nsca
+    j += 1
 
 # now run the configurations
 
 N = len(runlist)
 if Nmax is not None:
-    if N>Nmax:
-        N=Nmax
+    if Nmax < N:
+        N = Nmax
         runlist = runlist[:N]
-print(N, 'exposures')
+print(N, "exposures")
 sys.stdout.flush()
 
-# simulation function (to be given to workers)
+
 def f(r):
-    (j,c1,c2) = r
+    """Simulation function (to be given to workers)."""
+    (j, c1, c2) = r
     print(c1)
     sim_to_isim.run_config(c1)
     print(c2)
-    gen_noise_image.calibrateimage(c2 | {'SLICEOUT': True})
+    gen_noise_image.calibrateimage(c2 | {"SLICEOUT": True})
     gen_noise_image.generate_all_noise(c2)
-    print('write mask')
-    maskhandling.PixelMask1.convert_file(c2['OUT'], c2['OUT'][:-5]+'_mask.fits')
+    print("write mask")
+    maskhandling.PixelMask1.convert_file(c2["OUT"], c2["OUT"][:-5] + "_mask.fits")
+
 
 for r in runlist:
     f(r)
--- runs/summer2025run/makemask.py
+++ runs/summer2025run/makemask.py
@@ -1,53 +1,52 @@
+"""This script makes a mask file."""
+
 import sys
-import numpy as np
-import asdf
 from datetime import datetime, timezone
 
-"""This script makes a mask file."""
+import asdf
+import numpy as np
 
 outfile = sys.argv[1]
 sca = int(sys.argv[2])
 
-dq = np.zeros((4096,4096), dtype=np.uint32)
+dq = np.zeros((4096, 4096), dtype=np.uint32)
 
 # Reference pixels
-dq[:4,:] |= 2**31
-dq[-4:,:] |= 2**31
-dq[:,:4] |= 2**31
-dq[:,-4:] |= 2**31
+dq[:4, :] |= 2**31
+dq[-4:, :] |= 2**31
+dq[:, :4] |= 2**31
+dq[:, -4:] |= 2**31
 
 # Low QE pixels
-with asdf.open(outfile.replace('_mask_', '_linearitylegendre_')) as f:
-    pflat = f['roman']['pflat'][0,:,:]
+with asdf.open(outfile.replace("_mask_", "_linearitylegendre_")) as f:
+    pflat = f["roman"]["pflat"][0, :, :]
     print(np.median(pflat))
-    pflat = pflat/np.median(pflat)
-    dq |= f['roman']['dq']
-    dq |= np.where(pflat<.5, 2**13, 0).astype(np.uint32)
+    pflat = pflat / np.median(pflat)
+    dq |= f["roman"]["dq"]
+    dq |= np.where(pflat < 0.5, 2**13, 0).astype(np.uint32)
 
 # Hot pixels - right now 12.5 DN/s
 # Warm pixels - right now 0.25 DN/s
-with asdf.open(outfile.replace('_mask_', '_dark_')) as f:
-    darkslope = f['roman']['dark_slope']
-    dq |= np.where(darkslope>0.25, np.where(darkslope>12.5, 2**11, 2**12), 0).astype(np.uint32)
+with asdf.open(outfile.replace("_mask_", "_dark_")) as f:
+    darkslope = f["roman"]["dark_slope"]
+    dq |= np.where(darkslope > 0.25, np.where(darkslope > 12.5, 2**11, 2**12), 0).astype(np.uint32)
 
 
-tree = {'roman': {
-    'meta': {
-        'author': 'makemask.py',
-        'description': 'makemask.py',
-        'instrument': {
-            'detector': 'WFI{:02d}'.format(sca),
-            'name': 'WFI'
+tree = {
+    "roman": {
+        "meta": {
+            "author": "makemask.py",
+            "description": "makemask.py",
+            "instrument": {"detector": f"WFI{sca:02d}", "name": "WFI"},
+            "origin": "PIT - romanimpreprocess",
+            "date": datetime.now(timezone.utc).isoformat(),
+            "pedigree": "DUMMY",
+            "reftype": "PFLAT",
+            "telescope": "ROMAN",
+            "useafter": "!time/time-1.2.0 2020-01-01T00:00:00.000",
         },
-        'origin': 'PIT - romanimpreprocess',
-        'date': datetime.now(timezone.utc).isoformat(),
-        'pedigree': 'DUMMY',
-        'reftype': 'PFLAT',
-        'telescope': 'ROMAN',
-        'useafter': '!time/time-1.2.0 2020-01-01T00:00:00.000'
+        "dq": dq,
     },
-    'dq': dq
-},
 }
 
 asdf.AsdfFile(tree).write_to(outfile)
