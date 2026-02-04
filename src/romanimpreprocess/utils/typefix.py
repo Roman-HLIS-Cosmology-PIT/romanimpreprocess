d0e7a7fd0c62341d9d8c9b365aa60bd6ee958c43
hirata10 <hirata.10@osu.edu>
2026-02-03 22:16:36 -0500

    more fixes

--- src/romanimpreprocess/utils/typefix.py
+++ src/romanimpreprocess/utils/typefix.py
@@ -18,11 +18,23 @@ def fix(tree):
 
     """
 
-    try:
-        tree.validate()
-    except asdf._jsonschema.exceptions.ValidationError as ve:
-        # this is a common error
-        e = str(ve)
-        if "'err'" in e and "float16" in e and "err" in tree["roman"]:
-            print("Fixing ...", e)
-            tree["roman"]["err"] = tree["roman"]["err"].astype(np.float16)
+    max_retries = 4
+    for attempt in range(max_retries):
+        try:
+            tree.validate()
+        except asdf._jsonschema.exceptions.ValidationError as ve:
+            # this is a common error. move on if we get too many.
+            if attempt == max_retries - 1:
+                raise Exception("Validation error reached max tries") from e
+            e = str(ve)
+
+            # Now go through the possible changes we might have to make
+            if "'err'" in e and "float16" in e and "err" in tree["roman"]:
+                print("Fixing err ...")
+                tree["roman"]["err"] = tree["roman"]["err"].astype(np.float16)
+            if "'var_poisson'" in e and "float16" in e and "var_poisson" in tree["roman"]:
+                print("Fixing var_poisson ...")
+                tree["roman"]["var_poisson"] = tree["roman"]["var_poisson"].astype(np.float16)
+            if "'var_read'" in e and "float16" in e and "var_read" in tree["roman"]:
+                print("Fixing var_read ...")
+                tree["roman"]["var_read"] = tree["roman"]["var_read"].astype(np.float16)
