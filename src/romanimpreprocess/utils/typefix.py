b77c9294d18943d521c1ac4eefb94df78f87b826
hirata10 <hirata.10@osu.edu>
2026-02-03 21:04:07 -0500

    Create typefix.py

---
+++ src/romanimpreprocess/utils/typefix.py
@@ -0,0 +1,26 @@
+"""Simple fix to output type if we have a different schema."""
+
+import asdf
+import numpy as np
+
+def fix(tree):
+    """
+    Fixes a tree. More fixes can be put here.
+
+    Parameters
+    ----------
+    tree : AsdfTree
+        The ASDF tree to fix in place.
+
+    Returns
+    -------
+    None
+    
+    """
+
+    try:
+        tree.validate()
+        except asdf._jsonschema.exceptions.ValidationError as e:
+            # this is a common error
+            if "'err'" in e and "float16" in e and "err" in tree["roman"]:
+                tree["roman"]["err"] = tree["roman"]["err"].astype(np.float16)
