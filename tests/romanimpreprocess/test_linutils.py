21d386b312f34cba9bd747a8fb74dfe4e0c06a75
hirata10 <hirata.10@osu.edu>
2026-02-04 12:17:13 -0500

    Update test_linutils.py

--- tests/romanimpreprocess/test_linutils.py
+++ tests/romanimpreprocess/test_linutils.py
@@ -1,7 +1,6 @@
 """Test for simple linearity function."""
 
 import numpy as np
-
 from romanimpreprocess.utils.ipc_linearity import _lin
 
 
@@ -12,4 +11,8 @@ def test_lin():
     coefs[3, :, :] = 1.0
     phi, _ = _lin(z, coefs)
     print(phi)
-    assert np.all(phi < -1.0e6)
+    phidff = phi - np.array([-4., -3.4, -2.8, -2.2, -1.6, -1., -0.4725, -0.08, 0.1925,
+        0.36, 0.4375, 0.44, 0.3825, 0.28, 0.1475, 0., -0.1475, -0.28,
+       -0.3825, -0.44, -0.4375, -0.36, -0.1925, 0.08, 0.4725, 1., 1.6,
+       2.2, 2.8, 3.4, 4.]).reshape(np.shape(phi))
+    assert np.all(phidiff < -1.0e6)
