accc236d1935f584be1da38a4c33a70b94d533b6
hirata10 <hirata.10@osu.edu>
2026-02-05 14:49:17 -0500

    Create test_pearson.py
    This one is just a template.

---
+++ tests/romanimpreprocess/test_pearson.py
@@ -0,0 +1,12 @@
+"""Tests for the Pearson distributions."""
+
+import numpy as np
+
+from romanimpreprocess.L1_to_L2.GalPoisson.draw_with_tilnus import random_from_type5
+
+# def test_pearson5():
+#
+#     ...
+#
+#     assert -5.0 < mean < -4.0
+
