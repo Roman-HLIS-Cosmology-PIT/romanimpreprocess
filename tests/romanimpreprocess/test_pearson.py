d457ce7ba4a80c227f8565d24384701884b5a541
hirata10 <hirata.10@osu.edu>
2026-02-06 08:11:13 -0500

    unused import bypass in test_pearson.py
    I silenced ruff error F401 because we *will* use these imports (just not yet).

--- tests/romanimpreprocess/test_pearson.py
+++ tests/romanimpreprocess/test_pearson.py
@@ -1,8 +1,8 @@
 """Tests for the Pearson distributions."""
 
-import numpy as np
+import numpy as np  # noqa: F401
 
-from romanimpreprocess.L1_to_L2.GalPoisson.draw_with_tilnus import random_from_type5
+from romanimpreprocess.L1_to_L2.GalPoisson.draw_with_tilnus import random_from_type5  # noqa: F401
 
 # def test_pearson5():
 #
