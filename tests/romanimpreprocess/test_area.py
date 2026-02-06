887632f48cf0657c54563eb157b7570d9bcda622
hirata10 <hirata.10@osu.edu>
2026-02-05 20:06:55 -0500

    fixed test limits in test_area.py
    the previous version had a 2nd order error that exceeded the 1e-5 bound

--- tests/romanimpreprocess/test_area.py
+++ tests/romanimpreprocess/test_area.py
@@ -10,17 +10,19 @@ def test_area_astropy():
 
     # We'll test both hemispheres.
     for i in range(2):
+        N = 2000
+        d = 0.01
         w = WCS(naxis=2)
-        w.wcs.crpix = [100.5, 100.5]
-        w.wcs.cdelt = np.array([-0.1, 0.1])
+        w.wcs.crpix = [N / 2.0 + 0.5, N / 2.0 + 0.5]
+        w.wcs.cdelt = np.array([-d, d])
         w.wcs.crval = [25.0, 83.0 * (1. - 2. * i)]
         w.wcs.ctype = ["RA---STG", "DEC--STG"]
 
         area = pixelarea(w, N=200)
-        s = 0.1 * (np.linspace(0, 199, 200) - 99.5) * np.pi / 180.
+        s = d * (np.linspace(0, N-1, N) - N / 2.0 - 0.5) * np.pi / 180.
         x, y = np.meshgrid(s, s)
-        area_target = (0.1 * np.pi / 180.)**2 / (1. + (x**2 + y**2) / 4. )**2
+        area_target = (d * np.pi / 180.)**2 / (1. + (x**2 + y**2) / 4. )**2
         err = np.log(area / area_target)
-        print(err[::199, ::199])
-        print(err[99:101, 99:101])
-        assert np.all(np.abs(err) < 1.0e-5)
+        print(err)
+        print(err[N//2, N//2])
+        assert np.all(np.abs(err) < 2.0e-4)
