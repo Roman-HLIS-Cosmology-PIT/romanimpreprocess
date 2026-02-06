834fdad2d261452254f93c0fc131530625e667a2
hirata10 <hirata.10@osu.edu>
2026-02-06 08:15:38 -0500

    ruff reformat of test_pearson.py

--- tests/romanimpreprocess/test_pearson.py
+++ tests/romanimpreprocess/test_pearson.py
@@ -1,7 +1,6 @@
 """Tests for the Pearson distributions."""
 
 import numpy as np  # noqa: F401
-
 from romanimpreprocess.L1_to_L2.GalPoisson.draw_with_tilnus import random_from_type5  # noqa: F401
 
 # def test_pearson5():
